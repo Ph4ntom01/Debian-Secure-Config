7114d9d1568699f661772f06f520fe856d018a0e
Ph4ntom <b4rcode@pm.me>
2021-05-16 18:07:13 +0200

    Quality of life & Bug fixes
    - ssh part is more fluid
    - added the possibility to copy the private ssh key to a remote client
    - the script will only print usefull information

--- debian_secure_config.sh
+++ debian_secure_config.sh
@@ -1,26 +1,68 @@
 #!/bin/bash
 
+NC="\033[0m" # No Color
+RED="\033[0;31m"
+LRED="\033[1;31m"
+LPURPLE="\033[1;35m"
+
 SSH_CONF="/etc/ssh/sshd_config"
 SYSCTL_CONF="/etc/sysctl.conf"
 SOURCES_LIST="/etc/apt/sources.list"
+NULL="/dev/null"
 
 while true; do
     read -p "Which user do you want to configure ? " user
-    if id "$user" > /dev/null 2>&1; then
-        cd /home/${user}
-        break
-    else
-        echo "User does not exist."
+    user_id=$(id -u $user 2>/dev/null)
+    if [ -z $user_id ]; then
+        echo -e "User ${RED}$user${NC} does not exist."
         continue
+    else
+        break
     fi
 done
 
-echo -e "\nStarting configuration...\n"
-echo -e "Updating OS...\n"
-sleep 1
+SSH_KEYS="/home/${user}/.ssh"
 
-rm $SOURCES_LIST
-cat << EOF > $SOURCES_LIST
+echo -e "Starting configuration..."
+
+if [ ! -f "/usr/bin/sudo" ]; then
+    echo -e "\n${LRED}- SUDO -${NC}"
+    while true; do
+        read -p "Sudo is not installed, install sudo (y/n)? " response
+        if [ "$response" = "y" ]; then
+            apt-get install sudo -y >"$NULL" 2>&1
+            while true; do
+                read -p "Disable password request (y/n)? " response
+                if [ "$response" = "y" ]; then
+                    sed -i "s/.*\%sudo.*/\%sudo ALL=(ALL:ALL) NOPASSWD:ALL/g" /etc/sudoers
+                    break
+                elif [ "$response" = "n" ]; then
+                    break
+                fi
+            done
+            break
+        elif [ "$response" = "n" ]; then
+            break
+        fi
+    done
+fi
+
+if [ -z $(groups "$user" | grep -o "sudo") ]; then
+    while true; do
+        read -p "$user does not belong to the sudo group, add him (y/n)? " response
+        if [ "$response" = "y" ]; then
+            /usr/sbin/usermod -aG sudo "$user"
+            break
+        elif [ "$response" = "n" ]; then
+            break
+        fi
+    done
+fi
+
+echo -e "\n${LRED}- Updating OS & Installing Net Packages -${NC}"
+
+rm "$SOURCES_LIST"
+cat <<EOF >"$SOURCES_LIST"
 deb http://deb.debian.org/debian buster main
 deb-src http://deb.debian.org/debian buster main
 
@@ -31,56 +73,81 @@ deb http://deb.debian.org/debian buster-updates main
 deb-src http://deb.debian.org/debian buster-updates main
 EOF
 
-apt update && apt upgrade -y
-apt install openssh-server -y
-apt install dnsutils -y
-apt install net-tools -y
+apt-get update 1>"$NULL"
+apt-get upgrade -y 1>"$NULL"
+apt-get install openssh-server -y 1>"$NULL"
+apt-get install dnsutils -y 1>"$NULL"
+apt-get install net-tools -y 1>"$NULL"
 
-echo -e "OS updated.\n"
-echo -e "******************************************************************************\n"
+echo -e "OS Updated."
+echo -e "\n${LRED}- SSH -${NC}"
 
-while true; do
-    read -p "Install sudo (y/n)? " response
-    if [ "$response" = "y" ]; then
-        apt install sudo -y
-        echo -e "\n"
-        while true; do
-            read -p "Disable password request (y/n)? " response
-            if [ "$response" = "y" ]; then
-                sed -i "s/.*\%sudo.*/\%sudo ALL=(ALL:ALL) NOPASSWD:ALL/g" /etc/sudoers
-                break
-            elif [ "$response" = "n" ]; then break
+function ssh() {
+    sudo -u "$user" mkdir -p $SSH_KEYS
+    cd $SSH_KEYS
+    sudo -u "$user" touch known_hosts
+    sudo -u "$user" ssh-keygen -t rsa -b 4096 -f $user 1>"$NULL"
+    sudo -u "$user" cat ${user}.pub >authorized_keys
+    sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g" $SSH_CONF
+    sed -i "s/.*AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys/g" $SSH_CONF
+    echo -e "\n  - Your private key has been saved in ${LPURPLE}${SSH_KEYS}/${user}${NC}"
+    echo -e "  - Your public key has been saved in ${LPURPLE}${SSH_KEYS}/${user}.pub${NC}\n"
+    sleep 3
+    while true; do
+        read -p "Send the private key to a remote client (linux only) (y/n)? " response
+        if [ "$response" = "y" ]; then
+            read -p "Port: " scp_port
+            read -p "File location [${SSH_KEYS}/${user}]: " scp_file
+            scp_file=${scp_file:-${SSH_KEYS}/${user}}
+            read -p "Username: " scp_username
+            read -p "IP: " scp_ip
+            read -p "Remote destination: " scp_destination
+            if $(scp -P $scp_port $scp_file ${scp_username}@${scp_ip}:${scp_destination} >"$NULL" 2>&1); then
+                echo -e "${LPURPLE}The private key has been successfully sent! Follow theses steps to secure your server:${NC} "
+                echo -e "\n  - First, uncomment and set PasswordAuthentication to no in the /etc/ssh/sshd_config file."
+                echo -e "  - Second, remove the private key (${SSH_KEYS}/${user}).\n"
+            else
+                sleep 1
+                echo -e "\n${RED}An error occured, follow theses steps to get your private key:${NC} "
+                ssh_error
             fi
-        done
-        break
-    elif [ "$response" = "n" ]; then break
-    fi
-done
+            break
+        elif [ "$response" = "n" ]; then
+            echo -e "\n${LPURPLE}Follow theses steps to secure your server:${NC} "
+            ssh_error
+            break
+        fi
+    done
+    sleep 3
+    while true; do
+        read -p "Make ${SSH_KEYS} folder immutable (y/n)? " response
+        if [ "$response" = "y" ]; then
+            chattr -R +i $SSH_KEYS
+            break
+        elif [ "$response" = "n" ]; then
+            break
+        fi
+    done
+}
 
-echo -e "\n"
-
-while true; do
-    read -p "Add $user as a sudo user (y/n)? " response
-    if [ "$response" = "y" ]; then
-        adduser $user sudo
-        break
-    elif [ "$response" = "n" ]; then break
-    fi
-done
-
-echo -e "\n******************************************************************************\n"
+function ssh_error() {
+    sleep 1
+    echo -e "\n  - First, make sure you copy you private key (${SSH_KEYS}/${user}) to your client with a FTP software (ex: FileZilla)."
+    echo -e "  - Second, uncomment and set PasswordAuthentication to no in the /etc/ssh/sshd_config file."
+    echo -e "  - Third, remove the private key (${SSH_KEYS}/${user}).\n"
+    sleep 3
+}
 
 while true; do
     read -p "Modify SSH port (y/n)? " response
     if [ "$response" = "y" ]; then
-        read -p "Enter port for SSH: " ssh
+        read -p "Enter SSH port: " ssh
         sed -i "10,20 s/.*Port.*/Port="${ssh}"/g" $SSH_CONF
         break
-    elif [ "$response" = "n" ]; then break
+    elif [ "$response" = "n" ]; then
+        break
     fi
 done
-
-echo -e "\n"
 
 while true; do
     read -p "Permit root login (y/n)? " response
@@ -93,113 +160,117 @@ while true; do
     fi
 done
 
-echo -e "\n"
-
 while true; do
     read -p "Use SSH key (y/n)? " response
     if [ "$response" = "y" ]; then
-        mkdir -p .ssh && chown ${user}:${user} .ssh
-        cd .ssh
-        ssh-keygen -t rsa -b 4096 -f $user
-        mv $user id_rsa
-        mv ${user}.pub id_rsa.pub
-        cat id_rsa.pub > authorized_keys
-        chown ${user}:${user} id_rsa id_rsa.pub authorized_keys
-        sed -i "s/root/${user}/g" id_rsa.pub
-        sed -i "s/root/${user}/g" authorized_keys
-        sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication yes/g" $SSH_CONF
-        sed -i "s/.*AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys/g" $SSH_CONF
-        echo -e "\n"
-        echo -e "After the installation, make sure you copy you private key (/home/${user}/.ssh/id_rsa) to your client with a FTP software (ex: FileZilla).\n\n"
-        sleep 3
-        while true; do
-            read -p "Make .ssh folder immutable (y/n)? " response
-            if [ "$response" = "y" ]; then
-                cd ..
-                chattr -R +i .ssh
-                break
-            elif [ "$response" = "n" ]; then break
-            fi
-        done
+        if [ -d $SSH_KEYS ]; then
+            while true; do
+                read -p "There is already a configuration, would you like to erase it (y/n)? " response
+                if [ "$response" = "y" ]; then
+                    chattr -R -i $SSH_KEYS 2>"$NULL"
+                    rm -Rf $SSH_KEYS
+                    ssh
+                    break
+                elif [ "$response" = "n" ]; then
+                    break
+                fi
+            done
+            break
+        else
+            ssh
+            break
+        fi
+    elif [ "$response" = "n" ]; then
+        chattr -R -i $SSH_KEYS 2>"$NULL"
+        rm -Rf $SSH_KEYS
         break
-    elif [ "$response" = "n" ]; then break
     fi
 done
 
-service sshd restart
+systemctl restart sshd
 sleep 1
 
-echo -e "\n******************************************************************************\n"
+echo -e "\n${LRED}- IPv6 -${NC}"
 
 while true; do
     read -p "Disable IPv6 (y/n)? " response
     if [ "$response" = "y" ]; then
         sed -i "/net.ipv6.conf/d" $SYSCTL_CONF
-        echo "net.ipv6.conf.all.disable_ipv6=1" >> $SYSCTL_CONF
-        echo "net.ipv6.conf.default.disable_ipv6=1" >> $SYSCTL_CONF
-        echo "net.ipv6.conf.lo.disable_ipv6=1" >> $SYSCTL_CONF
+        echo "net.ipv6.conf.all.disable_ipv6 = 1" >>$SYSCTL_CONF
+        echo "net.ipv6.conf.default.disable_ipv6 = 1" >>$SYSCTL_CONF
+        echo "net.ipv6.conf.lo.disable_ipv6 = 1" >>$SYSCTL_CONF
         break
-    elif [ "$response" = "n" ]; then break
+    elif [ "$response" = "n" ]; then
+        # delete net.ipv6.conf lines
+        sed -i "/net.ipv6.conf/d" $SYSCTL_CONF
+        break
     fi
 done
 
-echo -e "\n"
+echo -e "\n${LRED}- UFW -${NC}"
 
 while true; do
     read -p "Install UFW firewall (y/n)? " response
     if [ "$response" = "y" ]; then
-        apt install ufw -y
-        echo -e "\n"
+        apt-get install ufw -y >"$NULL" 2>&1
         while true; do
             read -p "Allow SSH connection (y/n)? " response
             if [ "$response" = "y" ]; then
                 ssh=$(grep "Port=" $SSH_CONF | cut -c 6-11)
-                echo "SSH port is: "${ssh}
-                ufw allow $ssh
+                /usr/sbin/ufw allow $ssh 1>"$NULL"
                 break
-            elif [ "$response" = "n" ]; then break
+            elif [ "$response" = "n" ]; then
+                break
             fi
         done
         while true; do
-            echo -e "\n"
             read -p "Set up default policies (y/n)? " response
             if [ "$response" = "y" ]; then
-                ufw default deny incoming
-                ufw default allow outgoing
+                /usr/sbin/ufw default deny incoming 1>"$NULL"
+                /usr/sbin/ufw default allow outgoing 1>"$NULL"
+                echo -e "${LPURPLE}Default policies changed to: deny incoming, allow outgoing.${NC}"
                 break
-            elif [ "$response" = "n" ]; then break
+            elif [ "$response" = "n" ]; then
+                /usr/sbin/ufw default allow incoming 1>"$NULL"
+                /usr/sbin/ufw default allow outgoing 1>"$NULL"
+                echo -e "${LPURPLE}Default policies changed to: allow incoming, allow outgoing.${NC}"
+                break
             fi
         done
-        echo -e "\n"
         while true; do
-            read -p "Disable IPV6 (y/n) ? " response
+            read -p "Disable firewall IPv6 (y/n)? " response
             if [ "$response" = "y" ]; then
                 sed -i "s/.*IPV6.*/IPV6=no/g" /etc/default/ufw
                 break
-            elif [ "$response" = "n" ]; then break
+            elif [ "$response" = "n" ]; then
+                break
             fi
         done
-        echo -e "\nUFW Activation ..."
-        sleep 3
-        ufw enable
+        echo -e "UFW Activation..."
+        sleep 1
+        /usr/sbin/ufw enable 1>"$NULL"
+        echo -e "UFW Activated!"
         break
-    elif [ "$response" = "n" ]; then break
+    elif [ "$response" = "n" ]; then
+        /usr/sbin/ufw disable >"$NULL" 2>&1
+        break
     fi
 done
 
-echo -e "\n******************************************************************************\n"
+echo -e "\n${LRED}- Unattended Upgrades -${NC}"
 
 while true; do
-    read -p "Enable unattended upgrades ? " response
+    read -p "Install unattended upgrades (y/n)? " response
     if [ "$response" = "y" ]; then
-        sudo apt-get install unattended-upgrades apt-listchanges -y
-        echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
-        echo 'APT::Periodic::Unattended-Upgrade "1";' >> /etc/apt/apt.conf.d/20auto-upgrades
+        apt-get install unattended-upgrades apt-listchanges -y 1>"$NULL"
         break
-    elif [ "$response" = "n" ]; then break
+    elif [ "$response" = "n" ]; then
+        break
     fi
 done
 
-echo -e "\nDone. System must be restarted...\n"
+echo -e "\nEnding configuration..."
 sleep 1
-exit 0
+echo -e "System must be restarted."
+sleep 1
+exit
